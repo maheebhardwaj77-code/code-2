5885b4a75ad6ae85342ec9618a27028975ec7c81
samarth_bhardwaj <smarth.bhardwaj15@gmail.com>
2026-06-30 15:34:47 +0530

    Describe your change

--- coding2.py
+++ coding2.py
@@ -4,16 +4,29 @@ G = input("M OR F :")
 if((A == 1 or A == 2) and G == "M"):
     print("fee is 100")
 
-elif((A == 3 or A == 4) and G == "F"):
+elif(A == 3 or A == 4 or G == "F"):
     print("fee is 200")
 
 elif(A == 5 and G == "M"):
     print("fee is 300")
 
 else:
-    print("no fee")            
+    print("no fee") 
 
+#Single Line Conditional Statements:-
+age = int(input("age :"))
+vote = "yes can give vote" if age >= 18 else "no you can't give vote"
+print(vote)
 
+#Clever If statements:-
+marks = int(input("marks :"))
+result = ("fail", "pass")  [marks >= 40]
+print(result)
 
-
+#Type Casting:-
+a = 4
+b = "5"
+c = int(b)
+sum = a + c
+print(sum)
 
