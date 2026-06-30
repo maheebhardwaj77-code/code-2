4c39f448df0e31e8fc224bfb5878def83fb9150e
samarth_bhardwaj <smarth.bhardwaj15@gmail.com>
2026-06-30 10:10:28 +0530

    Updated project

---
+++ coding2.py
@@ -0,0 +1,19 @@
+print("hello guys")
+A = int(input("A :"))
+G = input("M OR F :")
+if((A == 1 or A == 2) and G == "M"):
+    print("fee is 100")
+
+elif((A == 3 or A == 4) and G == "F"):
+    print("fee is 200")
+
+elif(A == 5 and G == "M"):
+    print("fee is 300")
+
+else:
+    print("no fee")            
+
+
+
+
+
