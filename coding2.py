da74c1271a0e616c10ef10210f32bac0636c1fea
samarth_bhardwaj <smarth.bhardwaj15@gmail.com>
2026-06-30 17:59:30 +0530

    new update

--- coding2.py
+++ coding2.py
@@ -52,4 +52,61 @@ print("the average is :",average)
 #WAP to input 2 int number a and b. Print true if a is greater than or equal to b. If not print false:-
 first = int(input("first :"))
 second = int(input("second :"))
-print(first >= second)     
+print(first >= second)  
+
+#String and its rule:-
+str1 = "mahee" #concatenation in strings
+str2 = "bhardwaj"
+final = (str1 + " " + str2)
+print(final)
+len1 = len(str1) #length of string
+len2 = len(str2)
+final_len = len(final)
+print(len1)
+print(len2)
+print(final_len)
+final_ch = final[4] #indexing
+print(final_ch)
+print(final[2:9]) #slicing
+print(final[4:7])
+print(final[-11:-5])
+print(final[-10:-5])
+print(final.endswith("waj"))     #returns true if string ends with substr
+print(final.capitalize())        #capitalizes 1st char
+print(str1.replace("ee", "i"))   #replaces old value with new value
+print(str2.find("a"))            #return the index number of word
+print(final.count("a"))          #counts the occurrence of substr
+
+#WAP to input user's name and print its length:-
+user1 = input("user name :")
+user1_len = len(user1)
+print("length of user's name :", user1_len)
+
+#Nesting:-
+my_age = int(input("My age is :"))
+if(my_age >= 18):
+    if(my_age >=80):
+        print("over eaged")
+    else:
+        print("can drive")
+else:
+    print("minor")
+
+#WAP to check if a number entered by the user is odd or even:-
+number = int(input("enter the number :"))
+if(number % 2  == 0):
+    print("entered number is even")
+else:
+    print("entered number is odd")
+
+#WAP to find the greatest of 3 number entered by the user:-
+x = int(input("enter x :"))
+y = int(input("enter y :"))
+z = int(input('enter z :'))
+if(x > y):
+    if(x > z):
+        print("x is greatest number :",x)
+    else:
+        print("z is graetest number :",z)
+elif(y > z):
+    print("y is greatest number :",y)
